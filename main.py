2be2625d21b7089233b5847c2cee2708fe6cdcd6
KaranBhakuni <karanbhakuni21@gmail.com>
2025-10-12 18:37:19 +0530

    understood data validation

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):  # api ka schema ( datatype ) 
+    title: str
+    content: str
+
+#order of calling:1st method matched then r2 out match
 
 @app.get("/")
 def read_root():
@@ -12,3 +19,9 @@ def create_posts(payload: dict = Body(...)):    #body sare feilds ko nikal ke pa
     print(payload)
     # return {"message": "succesfully created posts"}
     return {"new_post": f"title{payload['title']} content{payload['content']}"}
+
+@app.post("/createposts")
+def create_posts(payload: Post):    # data validation Post class se 
+    print(payload.title)
+    # return {"message": "succesfully created posts"}
+    return {"new_post": "received"}
