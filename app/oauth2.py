eb7d35fb038efeae97ddd0de8eb8938a2878d4a3
KaranBhakuni <karanbhakuni21@gmail.com>
2025-11-06 08:56:24 +0530

    created login dependency

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,11 @@
-from jose import JWSError, jwt 
-from datetime import datetime, timedelta
+from jose import JWTError, jwt 
+from datetime import datetime, timedelta, timezone
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security.oauth2 import OAuth2PasswordBearer
+
+oauth2_scheme= OAuth2PasswordBearer(tokenUrl="login")  #used in logged in dependency
+
 
 #SECRET_KEY
 #HASH ALGO
@@ -12,9 +18,32 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode= data.copy()
 
-    expire= datetime.now() + timedelta (minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire= datetime.now(timezone.utc) + timedelta (minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode (to_encode,SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+def verify_access_token(token: str, credential_exception):
+
+    try:
+
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+
+        id:str = payload.get("user_id")
+
+        if id is None:
+            raise credential_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credential_exception
+    
+    return token_data
+
+# dependency , where user needs to be loged in to use that rout
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
