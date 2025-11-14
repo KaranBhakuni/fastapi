e79bcbca2bdcdd258fe79137a04bf7a314f9a23e
KaranBhakuni <karanbhakuni21@gmail.com>
2025-11-15 00:33:48 +0530

    added requirement file

--- app/main.py
+++ app/main.py
@@ -3,14 +3,27 @@ from .database import engine   # engine establish connection btw db and orm
 from . import models
 from .routers import posts, user, auth, vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 print(settings.database_username)
 
-models.Base.metadata.create_all(bind=engine)  # check model and if not created in db then create them, it not update them if we alter any column
+# now we are using alembic
+# models.Base.metadata.create_all(bind=engine)  # check model and if not created in db then create them, it not update them if we alter any column
 
 
 
 app = FastAPI()
+
+origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origns=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
 
 app.include_router(posts.router)
 app.include_router(user.router)
